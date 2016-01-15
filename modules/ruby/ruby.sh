04aeea5bd2696566aad0c44547df67031846dd76
Juan Ignacio Donoso <jidonoso@gmail.com>
2016-01-15 16:35:16 -0500

    update ruby module
    adds gem installation to the script itself. Now also the aliased version is set as global ruby

--- modules/ruby/ruby.sh
+++ modules/ruby/ruby.sh
@@ -10,12 +10,17 @@
 
 # Install the latest ruby version
 ruby_version="$(curl -sSL http://ruby.platan.us/latest)"
+ruby_version_alias="$(echo $ruby_version | cut -d "." -f -2)"
 if ! rbenv versions --bare | grep -Fq "$ruby_version"; then
   rbenv install -s "$ruby_version"
+  rbenv global "$ruby_version_alias"
 fi
 
-# Install or update gems defined in the gems file
-export RBENV_VERSION=$ruby_version
-while IFS='' read -r line || [ -n "$line" ]; do
-	gem_install_or_update "$line"
-done < $MODULE_PATH/gems
+export RBENV_VERSION=$ruby_version_alias
+
+# Install or update base gems
+gem_install_or_update 'bundler'
+gem_install_or_update 'negroku'
+gem_install_or_update 'potassium'
+gem_install_or_update 'powder'
+gem_install_or_update 'rubocop'
